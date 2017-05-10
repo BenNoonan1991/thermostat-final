8c939578088bd5ff0d9c210b79c2743b38f78c32
anaalta <suciuanamaria@yahoo.com>
2017-05-10 14:16:29 +0200

    added minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,6 +11,16 @@ describe ('Thermostat', function(){
 
   it("increases temperature with up", function() {
     thermostat.up();
-    expect(thermostat.temperature).toEqual(21)
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('decreases temperature with down', function(){
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
+  it('returns error if temperature is less than 10', function(){
+    for(var i = 0; i<11; i++){
+    thermostat.down();}
+    expect(thermostat.temperature).toEqual(10);
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat(){
 this.temperature = 20;
+this.minimumTemperature = 10;
 }
 
 Thermostat.prototype.temperature = function(){
@@ -8,4 +9,11 @@ return this.temperature;
 
 Thermostat.prototype.up = function () {
   this.temperature += 1;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.temperature === this.minimumTemperature){
+    return  this.minimumTemperature;
+  }
+    this.temperature -=1;
 };
