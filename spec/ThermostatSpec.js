1a65178b54ac94b087a82034aae8709316c4ca56
david gray <davidseangray@gmail.com>
2017-05-10 14:58:08 +0100

    pwersaving mode can turn on and off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -18,13 +18,25 @@ describe ('Thermostat', function(){
     thermostat.down();
     expect(thermostat.temperature).toEqual(19);
   });
-  it('returns error if temperature is less than 10', function(){
+  it('returns error if temperature is less than 10', function() {
     for(var i = 0; i<11; i++){
     thermostat.down();}
     expect(thermostat.temperature).toEqual(10);
   });
 
   it('powersaving mode is on by default', function() {
-    expect(thermostat.powerSavingModeOn).toBe(true);
+    expect(thermostat.powerSavingMode).toBe(true);
   });
+
+  it('can turn off powersaving mode', function() {
+    thermostat.powerSavingModeOff();
+    expect(thermostat.powerSavingMode).toBe(false)
+  });
+
+  // it('max temperature is 25 when PSM is on', function() {
+  //   thermostat.isMaxTemp();
+  //   for(var i = 0; i < 6; i++){
+  //   thermostat.up();}
+  //   expect(thermostat.temperature).toEqual(25);
+  // });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,9 @@
 function Thermostat(){
 this.temperature = 20;
 this.minimumTemperature = 10;
-this.powerSavingModeOn = true;
+this.powerSavingMode = true;
+this.MAX_TEMP_PSM_ON = 25;
+this.MAX_TEMP_PSM_OFF = 32;
 }
 
 Thermostat.prototype.temperature = function(){
@@ -18,3 +20,14 @@ Thermostat.prototype.down = function() {
   }
     this.temperature -=1;
 };
+
+Thermostat.prototype.powerSavingModeOff = function () {
+  this.powerSavingMode = false
+};
+
+// Thermostat.prototype.isMaxTemp = function () {
+//   if (this.powerSavingModeOn === true) {
+//     return this.MAX_TEMP_PSM_ON;
+//   }
+//   return this.temperature === this.MAX_TEMP_PSM_OFF;
+// };
