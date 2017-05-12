74ccd72c7ae8806cac1e76bf591f8c4d00423bcf
Paula <pmuldoon@umich.edu>
2017-05-12 15:33:14 +0100

    refactored displayWeather function

--- Interface.js
+++ Interface.js
@@ -33,13 +33,22 @@ $(document).ready(function() {
     updateTemperature();
   });
 
-  $('#select-city').submit(function(event){
-    event.preventDefault();
-    var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c0b6c7e54c92e31933df2561e6117e25&units=metric', function(data) {
+  // $('#select-city').submit(function(event){
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c0b6c7e54c92e31933df2561e6117e25&units=metric', function(data) {
+  //     $('#current-outdoor-temperature').text(data.main.temp);
+  //   });
+  // });
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=c0b6c7e54c92e31933df2561e6117e25';
+    var units = '&units=metric';
+    $.get(url + token + units, function (data) {
       $('#current-outdoor-temperature').text(data.main.temp);
     });
-  });
+  }
 
   $('#energy').text(thermostat.getEnergy);
 });
